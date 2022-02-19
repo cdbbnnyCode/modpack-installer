7a4d3fc3b146254727ebc9c882a8bcdfeda2238e
cdbbnny <cdbbnnycode@users.noreply.github.com>
2022-02-19 12:52:57 -0600

    add stats to clean.py

--- clean.py
+++ clean.py
@@ -12,23 +12,32 @@ def make_global(dir, gdir):
         elif os.path.exists(dir):
             os.remove(dir)
         os.symlink(os.path.abspath(gdir), dir, True)
+        return True
+    return False
 
 def main():
     print("Modpack Maintenance v1.0.0")
 
     mods = set()
 
+    moved = 0
+    deleted = 0
+
     for packdirn in os.listdir('packs/'):
         packdir = 'packs/' + packdirn
-        make_global(packdir + '/.minecraft/assets', 'global/assets')
-        
+        upd = make_global(packdir + '/.minecraft/assets', 'global/assets')
+        moved += upd
         for mod in os.listdir(packdir + '/.minecraft/mods'):
             mods.add(mod)
 
     for mod in os.listdir('.modcache'):
         if mod not in mods:
             print("cleaning up %s" % mod)
+            deleted += os.stat('.modcache/' + mod).st_size
             os.remove('.modcache/' + mod)
+
+    print("Done! Deleted %.3f MiB of mods and migrated %d data folders" % \
+        (deleted / 1048576, moved))
 
 if __name__ == "__main__":
     main()
